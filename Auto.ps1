2c985dd31cb60d5eaf9b6a5ff1277c61bfc5c6d1
vuongtuha <70053986+vuongtuha@users.noreply.github.com>
2025-11-02 13:14:28 +0700

    More automation
    auto MAS, change to focus WindowsXLite and XOS

--- Auto.ps1
+++ Auto.ps1
@@ -5,9 +5,9 @@ $RegKeyPath = "HKU:\Control Panel\International"
 Import-Module International
 Set-WinSystemLocale en-US
 Set-WinHomeLocation -GeoId 0xF2
-Set-WinUserLanguageList en-US -Force
 Start-Sleep 1
 Set-Culture en-GB
+Set-WinUserLanguageList en-US -Force
 foreach ($c in Get-NetAdapter) { write-host 'Setting DNS for' $c.interfaceName ; Set-DnsClientServerAddress -InterfaceIndex $c.interfaceindex -ServerAddresses ('8.8.8.8', '8.8.4.4') }
 $username = ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name).Split("\")[-1]
 Set-LocalUser -Name $username -FullName UltraMagnus
@@ -17,121 +17,87 @@ w32tm /register
 net start w32time
 w32tm /resync /nowait
 
+$env:desk=[Environment]::GetFolderPath("Desktop")
+
 #Fix-100-disk-hdd-fck-up
 # --- SYSTEM FILE CHECKER STARTED ---
-Write-Host "--- SYSTEM FILE CHECKER STARTED ---"
+#Write-Host "--- SYSTEM FILE CHECKER STARTED ---"
 
-sfc /scannow
+#sfc /scannow
 
-Write-Host ""
+#Write-Host ""
 
 # --- DISM SCAN STARTED ---
-Write-Host "--- DISM SCAN STARTED ---"
-Write-Host ""
+#Write-Host "--- DISM SCAN STARTED ---"
+#Write-Host ""
 
-DISM /Online /Cleanup-Image /ScanHealth
+#DISM /Online /Cleanup-Image /ScanHealth
 
-Write-Host ""
-Write-Host ""
+#Write-Host ""
+#Write-Host ""
 
 # --- DISM REPAIR STARTED ---
-Write-Host "--- DISM REPAIR STARTED ---"
-DISM /Online /Cleanup-Image /RestoreHealth
-Write-Host "Done..."
-Write-Host ""
+#Write-Host "--- DISM REPAIR STARTED ---"
+#DISM /Online /Cleanup-Image /RestoreHealth
+#Write-Host "Done..."
+#Write-Host ""
 
 # --- Temp File Removal Started ---
-Write-Host "--- Temp File Removal Started ---"
+#Write-Host "--- Temp File Removal Started ---"
 
-Remove-Item -Path "c:\windows\temp\*" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path "c:\windows\temp" -Force -Recurse -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Path "c:\windows\temp" -Force | Out-Null
+#Remove-Item -Path "c:\windows\temp\*" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\temp" -Force -Recurse -ErrorAction SilentlyContinue
+#New-Item -ItemType Directory -Path "c:\windows\temp" -Force | Out-Null
 
-Remove-Item -Path "C:\WINDOWS\Prefetch\*" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "C:\WINDOWS\Prefetch\*" -Force -Recurse -ErrorAction SilentlyContinue
 
-Remove-Item -Path "$env:TEMP\*" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path "$env:TEMP" -Force -Recurse -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Path "$env:TEMP" -Force | Out-Null
+#Remove-Item -Path "$env:TEMP\*" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "$env:TEMP" -Force -Recurse -ErrorAction SilentlyContinue
+#New-Item -ItemType Directory -Path "$env:TEMP" -Force | Out-Null
 
-Remove-Item -Path "c:\windows\tempor~1" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path "c:\windows\tempor~1\*" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\tempor~1" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\tempor~1\*" -Force -Recurse -ErrorAction SilentlyContinue
 
-Remove-Item -Path "c:\windows\temp" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path "c:\windows\temp\*" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\temp" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\temp\*" -Force -Recurse -ErrorAction SilentlyContinue
 
-Remove-Item -Path "c:\windows\tmp" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path "c:\windows\tmp\*" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\tmp" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\tmp\*" -Force -Recurse -ErrorAction SilentlyContinue
 
-Remove-Item -Path "c:\windows\ff*.tmp" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\ff*.tmp" -Force -Recurse -ErrorAction SilentlyContinue
 
-Remove-Item -Path "c:\windows\history" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path "c:\windows\history\*" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\history" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\history\*" -Force -Recurse -ErrorAction SilentlyContinue
 
-Remove-Item -Path "c:\windows\cookies" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path "c:\windows\cookies\*" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\cookies" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\cookies\*" -Force -Recurse -ErrorAction SilentlyContinue
 
-Remove-Item -Path "c:\windows\recent" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path "c:\windows\recent\*" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\recent" -Force -Recurse -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\windows\recent\*" -Force -Recurse -ErrorAction SilentlyContinue
 
-Remove-Item -Path "c:\WIN386.SWP" -Force -ErrorAction SilentlyContinue
+#Remove-Item -Path "c:\WIN386.SWP" -Force -ErrorAction SilentlyContinue
 
-Write-Host "Done!"
-Write-Host ""
+#Write-Host "Done!"
+#Write-Host ""
 
 ###
 #Pronounce check
 Install-PackageProvider -Name NuGet -Force | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+winget upgrade --all --silent --accept-source-agreements --accept-package-agreements
 Repair-WinGetPackageManager -IncludePrerelease -AllUsers
-
-# Check for Windows 10 based on major version number (no matter anymore)
-#Get-AppxPackage -allusers Microsoft.WindowsStore | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
-#Set-ExecutionPolicy Bypass -Scope Process -Confirm:$False
-#if ($null -eq (Get-Command "winget.exe" -ErrorAction SilentlyContinue)) 
-#{ 
-#    Write-Output "WinGet is not present on the system"
-#    if (!(Get-Command -Verb Repair -Noun WinGetPackageManager)) {
-#        Write-Output "Microsoft.WinGet.Client is not installed or is not on the latest version"
-#        try
-#        {
-#            Write-Output "Attempting to uninstall an older version of Microsoft.WinGet.Client..."
-#            Uninstall-Module -Name Microsoft.WinGet.Client -Confirm:$false -Force -Scope CurrentUser    
-#        }
-#        catch 
-#        {
-#            Write-Output "Microsoft.WinGet.Client was not installed."
-#        }
-#        Write-Output "Installing Microsoft.WinGet.Client..."
-#        Install-PackageProvider -Name NuGet -Force -Confirm:$false -Scope CurrentUser
-#        Install-Module -Name Microsoft.WinGet.Client -Confirm:$false -Force -Scope CurrentUser
-#        Write-Output "Microsoft.WinGet.Client was installed successfully"
-#    }
-#
-#    Write-Output "Checking for updates for Microsoft.WinGet.Client module..."
-#    if ((Get-Module -Name Microsoft.WinGet.Client -ListAvailable).Version -ge '1.11.230')
-#    {
-#        Write-Output "Microsoft.WinGet.Client is up-to-date"
-#    } else {
-#        Write-Output "Updating Microsoft.WinGet.Client module..."
-#        Update-Module -Name Microsoft.WinGet.Client -Confirm:$false -Force -Scope CurrentUser
-#    }
-#
-#    Write-Output "Installing WinGet..."
-#    Repair-WinGetPackageManager
-#    Write-Output "WinGet was installed successfully"
-#}
 
 
 #office365 install
 if (!(Test-Path office.exe)) {
-curl -Uri "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x64&language=en-us&version=O16GA" -o office.exe
+curl -Uri "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=Professional2021Retail&platform=x64&language=en-us&version=O16GA" -o office.exe
 Start-Process -FilePath "office.exe" -WindowStyle Hidden
 }
 
 # Pretty useless stuff
 Import-Module Appx
-curl -Uri "https://github.com/vuongtuha/autoinitwindows/blob/main/xp.jpg?raw=true" -o "C:\ProgramData\Microsoft\User Account Pictures\xp.jpg"
-curl -Uri "https://github.com/vuongtuha/autoinitwindows/blob/main/user-192.png?raw=true" -o "C:\ProgramData\Microsoft\User Account Pictures\user-192.png"
+curl -Uri "" -o "C:\ProgramData\Microsoft\User Account Pictures\xp.jpg"
+curl -Uri "" -o "C:\ProgramData\Microsoft\User Account Pictures\user-192.png"
 $uap = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
 
 $lsw = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP'
@@ -195,7 +161,7 @@ function Install-OptionalPackage {
 ###
 
 # Call function to install required package
-$listOfStrings = "Giorgiotani.Peazip", "Microsoft.DotNet.Framework.DeveloperPack_4", "Microsoft.VCRedist.2015+.x64", "Microsoft.DirectX", "CocCoc.CocCoc", "lamquangminh.EVKey", "Daum.PotPlayer", "PeterPawlowski.foobar2000", "Faststone.Viewer", "SumatraPDF.SumatraPDF", "CodeSector.TeraCopy", "HiBitSoftware.HiBitUninstaller"
+$listOfStrings = "Giorgiotani.Peazip", "Microsoft.DotNet.Framework.DeveloperPack_4", "Microsoft.VCRedist.2015+.x64", "Microsoft.DirectX", "CocCoc.CocCoc", "UniKey.UniKey", "Daum.PotPlayer", "PeterPawlowski.foobar2000", "Faststone.Viewer", "SumatraPDF.SumatraPDF", "CodeSector.TeraCopy", "y", "Daum.PotPlayer", "PeterPawlowski.foobar2000", "Faststone.Viewer", "SumatraPDF.SumatraPDF", "CodeSector.TeraCopy", "REALiX.HWiNFO", "HiBitSoftware.HiBitUninstaller"
 Install-RequiredPackage -StringList $listOfStrings
 
 # Call function for optional packages
@@ -207,9 +173,11 @@ Install-OptionalPackage "Guru3D.RTSS"
 Install-OptionalPackage "StartIsBack.StartAllBack"
 Install-OptionalPackage "TeamViewer.TeamViewer"
 # Open PowerShell instance
+& "C:\Program Files\PeaZip\peazip.exe" -ext2smart $env:desk\driver.tar.gz
+& "env:desk\\driver\StartAllBack.exe"
 Start-Process PowerShell.exe -ArgumentList "-NoExit", "-Command", "& { irm https://christitus.com/win | iex }" -WindowStyle Hidden
-Start-Process PowerShell.exe -ArgumentList "-NoExit", "-Command", "& { irm https://massgrave.dev/get | iex }" -WindowStyle Hidden
-Start-Process "$env:LOCALAPPDATA\Microsoft\WinGet\Packages\lamquangminh.EVKey_Microsoft.Winget.Source_8wekyb3d8bbwe\EVKey64" -WindowStyle Hidden
+Start-Process PowerShell.exe -ArgumentList "-NoExit", "-Command", "& { & ([ScriptBlock]::Create((curl.exe -s --doh-url https://1.1.1.1/dns-query https://get.activated.win | Out-String))) /HWID /Ohook }" -WindowStyle Hidden
+Start-Process "$env:LOCALAPPDATA\Microsoft\WinGet\Packages\UniKey.UniKey_Microsoft.Winget.Source_8wekyb3d8bbwe\UniKeyNT" -WindowStyle Hidden
 Get-ChildItem -Path ([Environment]::GetFolderPath("MyDocuments")) -Recurse -dir | foreach { Remove-Item -Force -Recurse -Path $_}
 Get-ChildItem -Path ([Environment]::GetFolderPath("MyDocuments")) -file | Where-Object {$_.Name -NotContains "office.exe"} | Remove-Item -Force -Recurse
 regsvr32 "$env:ProgramFiles\TeraCopy\TeraCopy.dll"
